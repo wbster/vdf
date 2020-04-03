3da2ba0e720cb8815ccb025a98a2c35daf9ca7ac
max <wencorsman@gmail.com>
2020-04-03 03:46:18 +0300

    first commit

---
+++ index.js
@@ -0,0 +1,51 @@
+
+// const regExp = /(\s*)?"(?<key>[^"]+)"(\s+)?("(?<value>[^"]+)"$)?/;
+const regExp = /^(\s*)?"(?<k>[^"]+)"(\s*)?((?<v>.+))?(\s*)?(\[.*\])?(\s*)?(\/\/.*)?(\s*)?$/i
+
+function parser(strings) {
+    const data = {}
+    while (true) {
+        const string = strings.shift()
+        if (string === undefined) return data
+        // string = string.replace(/\s*\/\/.*$/, '')
+        const clear = string.replace(/\s+/g, ' ').trim()
+        if (clear == '}') return data
+        if (clear != '{') {
+            if (clear[0] == '$') {
+                const args = clear.replace(/\s*\/\/.*$/, '').split(' ')
+                const k = args.shift()
+                const v = args.join(' ').replace(/\"/g, '').replace(/\\+/g, '/')
+                data[k] = v
+                continue
+            }
+
+            if (clear.split(' ')[0] == 'include') {
+                if (!data.includes) data.includes = []
+                data.includes.push(clear.replace(/^\s*include\s*/i, ''))
+            }
+
+            const match = regExp.exec(string)
+            if (!match) continue
+            // @ts-ignore
+            const { groups: { k, v } } = match
+            // console.log('key, value', k, v)
+            if (!v) {
+                if (!data[k]) data[k] = {}
+                Object.assign(data[k], parser(strings))
+            } else {
+                data[k] = v.replace(/\\+/g, '/').replace(/\"/g, '')
+            }
+        }
+    }
+}
+
+/**
+ * parse vmt to obj
+ * @param {String} text 
+ * @returns {object}
+ */
+function parse(text) {
+    return parser(text.split('\n'))
+}
+
+exports.parse = parse
